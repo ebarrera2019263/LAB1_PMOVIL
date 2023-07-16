7ed24b19663890646f8088ec6a21a7ce76bfa3bf
ebarrera2019263 <ebarrera-2019263@kinal.edu.gt>
2023-07-16 17:50:08 -0600

    se agregaron los test

--- Laboratorio1_Plataformas/src/test/kotlin/ItemDataTest.kt
+++ Laboratorio1_Plataformas/src/test/kotlin/ItemDataTest.kt
@@ -4,6 +4,41 @@ import org.junit.Test
 
 class ItemDataTest {
 
+
+
+    @Test
+    fun `Given a list of elements in Java, When it has multiple elements, Then result type and info should be correct`() {
+        val result = listOf(
+            ItemDataJ(10),
+            ItemDataJ("Enero"),
+            ItemDataJ(null),
+            ItemDataJ(true)
+        )
+
+        assertNotNull(result)
+        assertTrue(
+            result[0].getType() == "entero"
+                    && result[0].getInfo() == "M10"
+        )
+
+        assertTrue(
+            result[1].getType() == "cadena"
+                    && result[1].getInfo() == "L5"
+        )
+        assertTrue(
+            result[2].getType() == null
+                    && result[2].getInfo() == null
+        )
+
+        assertTrue(
+            result[3].getType() == "booleano"
+                    && result[3].getInfo() == "Verdadero"
+        )
+    }
+
+
+
+
     @Test
     fun `Given a list, When it has multiple elements, Then result type and info should be correct`() {
         val result = listOf(
@@ -33,4 +68,6 @@ class ItemDataTest {
                     && result[3].info == "Verdadero"
         )
     }
+
+
 }
